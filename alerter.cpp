a2fd9717156069ac85e2891ae8d1dac9c82a190c
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-01-23 23:18:20 +0530

    Add tests to catch the network error bug

--- alerter.cpp
+++ alerter.cpp
@@ -9,9 +9,10 @@ int networkAlertStub(float celcius) {
     // Return 500 for not-ok
     // stub always succeeds and returns 200
     return 200;
+
 }
 
-void alertInCelcius(float farenheit) {
+int alertInCelcius(float farenheit, bool isNetworkOk) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
@@ -20,12 +21,13 @@ void alertInCelcius(float farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
-    }
+    } 
+    return alertFailureCount;
 }
 
-int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
+int main() {  
+    assert(alertInCelcius(400.5, false) != 0);
+    assert(alertInCelcius(303.6, false) != 0);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
