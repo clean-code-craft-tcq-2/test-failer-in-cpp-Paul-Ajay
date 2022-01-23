5685a02015b48849028171f846eb2420aa238c2d
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-01-23 23:25:58 +0530

    fix unused variable warning

--- alerter.cpp
+++ alerter.cpp
@@ -3,18 +3,21 @@
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
+int networkAlertStub(float celcius, bool isNetworkOk) {
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    if (isNetworkOk)
+        std::cout << "Network is ok" << std::endl;
+    else
+        std::cout << "Network is not-ok" << std::endl;
     return 200;
-
 }
 
 int alertInCelcius(float farenheit, bool isNetworkOk) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = networkAlertStub(celcius, isNetworkOk);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
