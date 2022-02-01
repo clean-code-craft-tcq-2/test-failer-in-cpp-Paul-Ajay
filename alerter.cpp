a222c80d25eca4396eef14735915e3e4f3c47b74
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2022-02-01 22:55:55 +0530

    fix unused varable issues

--- alerter.cpp
+++ alerter.cpp
@@ -6,13 +6,22 @@ int alertFailureCount = 0;
 class networkAlert
 {
 public:
-    virtual int checkNetworkStatus(bool isNetworkOk) = 0;
+    virtual int checkNetworkStatus(float celcius, bool isNetworkOk) = 0;
+    void printTempratureInCelcius() {
+        std::cout << "ALERT: Temperature is " << this->celcius << " celcius.\n";
+    }
+    float convertFarenheitToCelcius(float farenheit) {
+        return ((farenheit - 32) * 5 / 9);
+    }
+    float celcius;
 };
 
 class networkAlertStub : public networkAlert
 {
 public:
-    int checkNetworkStatus(bool isNetworkOk) {
+    int checkNetworkStatus(float celcius, bool isNetworkOk) {
+        this->celcius = celcius;
+        printTempratureInCelcius();
         if (isNetworkOk) {
             return 200;
         }
@@ -25,19 +34,23 @@ public:
 class networkAlertImpl : public networkAlert
 {
 public:
-    int checkNetworkStatus(bool isNetworkOk) {
-        return 200;
+    int checkNetworkStatus(float celcius, bool isNetworkOk) {
+        this->celcius = celcius;
+        printTempratureInCelcius();
+        if (isNetworkOk) {
+            return 200;
+        }
+        else {
+            return 500;
+        }
     }
 
 };
 
-float convertFarenheitToCelcius(float farenheit) {
-    return ((farenheit - 32) * 5 / 9);
-}
 
 int alertInCelcius(float farenheit, networkAlert* networkAlertObject, bool isNetworkOk) {
-    float celcius = convertFarenheitToCelcius(farenheit);
-    int returnCode = networkAlertObject->checkNetworkStatus(isNetworkOk);
+    float celcius = networkAlertObject->convertFarenheitToCelcius(farenheit);
+    int returnCode = networkAlertObject->checkNetworkStatus(celcius, isNetworkOk);
     if (returnCode != 200) {
         alertFailureCount += 1;
     } 
@@ -49,7 +62,7 @@ int main() {
     networkAlert * networkAlertStubObject = new networkAlertStub();
     assert(alertInCelcius(400.5, networkAlertStubObject, false) == 1);
     assert(alertInCelcius(303.6, networkAlertStubObject, true) == 1);
-    assert(alertInCelcius(303.6, networkAlertStubObject, false) == 2);
+    assert(alertInCelcius(97.4, networkAlertStubObject, false) == 2);
     std::cout << alertFailureCount << " alerts failed.\n";
     assert(alertFailureCount == 2);
     std::cout << "All is well (maybe!)\n";
